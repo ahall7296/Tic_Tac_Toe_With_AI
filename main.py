f58716c2c908368d67decc78d78a704dd8e1fca0
Anthony Hall <anthonyhall@Anthonys-MacBook-Pro.local>
2023-07-16 00:04:07 -0400

    Updated the playerMove function.

--- main.py
+++ main.py
@@ -23,7 +23,21 @@ def isWinner(bo, le):
     return (bo[7] == le and bo[8] == le and bo[9] ==le) or (bo[4] == le and bo[5] == le and bo[6] ==le) or (bo[1] == le and bo[2] == le and bo[3] ==le) or (bo[1] == le and bo[4] == le and bo[7] ==le) or (bo[2] == le and bo[5] == le and bo[8] ==le) or (bo[3] == le and bo[6] == le and bo[9] ==le) or (bo[1] == le and bo[5] == le and bo[9] ==le) or (bo[3] == le and bo[5] == le and bo[7] ==le)
 
 def playerMove():
-    pass
+    run = True
+    while run:
+        move = input('Please select a position to place an \'X\'(1-9): ')
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter('X', move)
+                else:
+                    print('Sorry, this space is occupied')
+            else:
+                print ('Please type a number within the range.')
+        except:
+            print('Please type a number')
 
 def compMove():
     pass
