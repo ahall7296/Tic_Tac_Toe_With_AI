79be2ece0209e41420d6d03cda2aa8ecf6e8896b
Anthony Hall <anthonyhall@Anthonys-MacBook-Pro.local>
2023-07-16 12:54:21 -0400

    Updated compMove fucntion.

--- main.py
+++ main.py
@@ -46,7 +46,36 @@ def selectRandom(li):
     return li[r]
 
 def compMove():
-    pass
+    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x !=0]
+    move = 0
+
+    for let in ['O', 'X']:
+        for i in possibleMoves:
+            boardCopy = board[:]
+            boardCopy[i] = let
+            if isWinner(boardCopy, let):
+                move = i
+                return move
+            
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1,3,7,9]:
+            cornersOpen.append(i)
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+    
+    if 5 in possibleMoves:
+        move=5
+        return move
+    
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2,4,6,8]:
+            edgesOpen.append(i)
+    if len(edgesOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
 
 def delectRandom(board):
     pass
@@ -70,8 +99,13 @@ def main():
             break
 
         if not(isWinner(board,  'X')):
-            compMove()
-            printBoard()
+            move = compMove()
+            if move == 0:
+                print('Game is a Tie! No more spaces left to move.')
+            else:
+                insertLetter('O', move)
+                print('Computer placed an \'O\' in position', move, ':')
+                printBoard()
         else:
             print('Great Job, You Won!')
             break
