305a2ed8c371cd1a261b82c235b5368a40247276
Anthony Hall <anthonyhall@Anthonys-MacBook-Pro.local>
2023-07-15 20:18:04 -0400

    Defined initial classes.

---
+++ main.py
@@ -0,0 +1,30 @@
+board = [' 'for x in range (10)]
+
+def insertLetter(letter, pos):
+    pass
+
+def spaceIsFree(pos):
+    pass
+
+def printBoard(board):
+    pass
+
+def isWinner(boe, le):
+    pass
+
+def playerMove():
+    pass
+
+def compMove():
+    pass
+
+def delectRandom(board):
+    pass
+
+def isBoardFull(board):
+    pass
+
+def main():
+    pass
+
+main()
