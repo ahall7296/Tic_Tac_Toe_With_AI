6c73e20ba1741feb6b9c82069712ac27393f4f17
Anthony Hall <anthonyhall@Anthonys-MacBook-Pro.local>
2023-07-15 23:13:20 -0400

    Defined board layout and main while loops.

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 board = [' 'for x in range (10)]
 
 def insertLetter(letter, pos):
-    pass
+    board[pos] = letter
 
 def spaceIsFree(pos):
-    pass
+    return board[pos] == ' '
 
 def printBoard(board):
-    pass
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
 
-def isWinner(boe, le):
-    pass
+def isWinner(bo, le):
+    return (bo[7] == le and bo[8] == le and bo[9] ==le) or (bo[4] == le and bo[5] == le and bo[6] ==le) or (bo[1] == le and bo[2] == le and bo[3] ==le) or (bo[1] == le and bo[4] == le and bo[7] ==le) or (bo[2] == le and bo[5] == le and bo[8] ==le) or (bo[3] == le and bo[6] == le and bo[9] ==le) or (bo[1] == le and bo[5] == le and bo[9] ==le) or (bo[3] == le and bo[5] == le and bo[7] ==le)
 
 def playerMove():
     pass
@@ -25,6 +35,25 @@ def isBoardFull(board):
     pass
 
 def main():
-    pass
+    print ('Thanks for playing our Tic-Tac-Toe game!')
+    printBoard()
+
+    while not(isBoardFull(board)):
+        if not(isWinner(board,  'O')):
+            playerMove()
+            printBoard()
+        else:
+            print('Sorry, you lost!')
+            break
+
+        if not(isWinner(board,  'X')):
+            compMove()
+            printBoard()
+        else:
+            print('Great Job, You Won!')
+            break
+
+    if isBoardFull (board):
+        print('Tie Game!')
 
 main()
